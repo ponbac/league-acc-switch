41fa1a4989a0f3c518f8253b71e58eefac44cc53
ponbac <root@ponbac.xyz>
2023-10-28 17:08:51 +0200

    list league processes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use specta::collect_types;
+use sysinfo::{ProcessExt, SystemExt};
 use tauri_specta::ts;
 
 mod commands;
@@ -14,4 +15,34 @@ fn main() {
         .invoke_handler(tauri::generate_handler![commands::hello,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn league_processes() -> Vec<(String, String)> {
+    let mut processes = Vec::new();
+
+    let sys = sysinfo::System::new_all();
+    for (pid, process) in sys.processes() {
+        let lowercase_name = process.name().to_lowercase();
+        if lowercase_name.contains("riot") || lowercase_name.contains("league") {
+            processes.push((pid.to_string(), process.name().to_string()));
+        }
+    }
+
+    processes
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_list_processes() {
+        let processes = league_processes();
+
+        for process in &processes {
+            println!("{}: {}", process.0, process.1);
+        }
+
+        assert!(!processes.is_empty());
+    }
 }
