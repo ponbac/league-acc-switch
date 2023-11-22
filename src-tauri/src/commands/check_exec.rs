49405f962aa2dec95465e704a5d8d9ff96d51456
ponbac <root@ponbac.xyz>
2023-11-22 11:05:24 +0100

    maybe updater

--- src-tauri/src/commands/check_exec.rs
+++ src-tauri/src/commands/check_exec.rs
@@ -1,9 +1,9 @@
-use std::path::Path;
+use std::{ffi::OsStr, path::Path};
 
 #[tauri::command]
 #[specta::specta]
 pub fn check_exec(client_exec_path: String) -> bool {
     let exec_path = Path::new(&client_exec_path);
 
-    exec_path.exists()
+    exec_path.file_name() == Some(OsStr::new("RiotClientServices.exe"))
 }
